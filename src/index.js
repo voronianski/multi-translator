d16aec69af385a53fd927eb29ec8feebf9c41c07
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2018-04-15 19:27:06 +0200

    add ui

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const { port, env } = require('c0nfig');
 
 const errors = require('./errors');
 const translate = require('./translate');
+const ui = require('./ui');
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.disable('x-powered-by');
 app.use(compression());
 
 app.get('/translate', translate);
+app.get('/ui', ui);
 
 app.use(errors.notfound);
 app.use(errors.handler);
---
+++ src/ui.js
@@ -0,0 +1,3 @@
+module.exports = function ui (req, res) {
+
+};
